36d93e7eed1332f91b48a16ea122fa1f3a910722
Davit Barbakadze <jayarjo@gmail.com>
2013-01-16 00:48:18 +0400

    HTML4, XMLHttpRequest: Properly abort the request and cleanup after it.

--- src/javascript/runtime/html4/xhr/XMLHttpRequest.js
+++ src/javascript/runtime/html4/xhr/XMLHttpRequest.js
@@ -1,9 +1,52 @@
-define("runtime/html4/xhr/XMLHttpRequest", ["o", "file/Blob", "xhr/FormData"], function(o, Blob, FormData) {
+define("runtime/html4/xhr/XMLHttpRequest", ["o", "core/utils/events", "file/Blob", "xhr/FormData"], function(o, events, Blob, FormData) {
 
 	var x = o.Exceptions;
 
 	return function() {
 		var _status, _response, _iframe;
+
+		function cleanup(cb) {
+			var target = this, uid, form, inputs, i, hasFile = false;
+
+			if (!_iframe) {
+				return;
+			}
+
+			uid = _iframe.id.replace(/_iframe$/, '');
+
+			form = o(uid + '_form');
+			if (form) {
+				inputs = form.getElementsByTagName('input');
+				i = inputs.length;
+				
+				while (i--) {
+					switch (inputs[i].getAttribute('type')) {
+						case 'hidden':
+							inputs[i].parentNode.removeChild(inputs[i]);
+							break;
+						case 'file':
+							hasFile = true; // flag the case for later
+							break;
+					}
+					inputs[i] = null;
+				}
+
+				if (!hasFile) { // we need to keep the form for sake of possible retries
+					form.parentNode.removeChild(form);
+				}
+				form = null;
+			}
+
+			// without timeout, request is marked as canceled (in console)
+			setTimeout(function() { 
+				events.removeEvent(_iframe, 'load', target.uid);
+				if (_iframe.parentNode) { // #382
+					_iframe.parentNode.removeChild(_iframe);
+				}
+				_iframe = null; 
+				cb();
+			}, 1);
+		}
 
 		o.extend(this, {
 				
@@ -26,7 +69,7 @@ define("runtime/html4/xhr/XMLHttpRequest", ["o", "file/Blob", "xhr/FormData"], f
 						console.info(_iframe.readyState);
 					};*/
 
-					o.addEvent(_iframe, 'load', function(e) { // _iframe.onload doesn't work in IE lte 8
+					events.addEvent(_iframe, 'load', function(e) { // _iframe.onload doesn't work in IE lte 8
 						var el;
 
 						try {
@@ -50,37 +93,14 @@ define("runtime/html4/xhr/XMLHttpRequest", ["o", "file/Blob", "xhr/FormData"], f
 						// get result 
 						_response = o.trim(el.body.innerHTML);
 
-						// cleanup
-						if (!data._blob) {
-							form.parentNode.removeChild(form);
-						} else {
-							(function() {
-								var inputs = form.getElementsByTagName('input'), i = inputs.length;
-								while (--i) {
-									if ('hidden' === inputs[i].getAttribute('type')) {
-										inputs[i].parentNode.removeChild(inputs[i]);
-									}
-									inputs[i] = null;
-								}
-							})();
-						}
-						form = null;
-
-						// without timeout, request is marked as canceled (in console)
-						setTimeout(function() { 
-							o.removeEvent(_iframe, 'load', target.uid);
-							if (_iframe.parentNode) { // #382
-								_iframe.parentNode.removeChild(_iframe);
-							}
-							_iframe = null; 
-
+						cleanup.call(this, function() {
 							target.trigger({
 								type: 'uploadprogress',
 								loaded: blob && blob.size || 1025,
 								total: blob && blob.size || 1025
 							});
 							target.trigger('load');
-						}, 1);
+						});
 					}, target.uid);
 				} // end createIframe
 
@@ -153,24 +173,22 @@ define("runtime/html4/xhr/XMLHttpRequest", ["o", "file/Blob", "xhr/FormData"], f
 			},
 
 			abort: function(upload_complete_flag) {
-				if (_iframe) {
-					if (o.typeOf(_iframe.stop) === 'function') { // FireFox/Safari/Chrome
-						_iframe.stop();
+				var target = this;
+
+				if (_iframe && _iframe.contentWindow) {
+					if (_iframe.contentWindow.stop) { // FireFox/Safari/Chrome
+						_iframe.contentWindow.stop();
+					} else if (_iframe.contentWindow.document.execCommand) { // IE
+						_iframe.contentWindow.document.execCommand('Stop');
 					} else {
-						_iframe.document.execCommand('Stop'); // IE
+						_iframe.src = "about:blank";
 					}
 				}
 
-				this.dispatchEvent('readystatechange');
-				// this.dispatchEvent('progress');
-				this.dispatchEvent('abort');
-				this.dispatchEvent('loadend');
-
-				if (!upload_complete_flag) {
-					// this.dispatchEvent('progress');
-					this.upload.dispatchEvent('abort');
-					this.upload.dispatchEvent('loadend');
-				}
+				cleanup.call(this, function() {
+					// target.dispatchEvent('readystatechange');
+					target.dispatchEvent('abort');
+				});
 			}
 		});
 	};
