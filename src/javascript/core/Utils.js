c51b6810dee68e06f04a3b427ed5aec7e95a9a28
Davit Barbakadze <jayarjo@gmail.com>
2013-01-16 18:24:10 +0400

    Expose addMimeType method to allow users to externally extend internal mimeType-to-extension lookup map.

--- src/javascript/core/Utils.js
+++ src/javascript/core/Utils.js
@@ -472,6 +472,134 @@ o.extend(o, {
 	}
 });	
 
+// Internal lookup maps for mime types and extensions
+(function(){ 
+	var mimeData = "" +
+		"application/msword,doc dot," +
+		"application/pdf,pdf," +
+		"application/pgp-signature,pgp," +
+		"application/postscript,ps ai eps," +
+		"application/rtf,rtf," +
+		"application/vnd.ms-excel,xls xlb," +
+		"application/vnd.ms-powerpoint,ppt pps pot," +
+		"application/zip,zip," +
+		"application/x-shockwave-flash,swf swfl," +
+		"application/vnd.openxmlformats-officedocument.wordprocessingml.document,docx," +
+		"application/vnd.openxmlformats-officedocument.wordprocessingml.template,dotx," +
+		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,xlsx," +
+		"application/vnd.openxmlformats-officedocument.presentationml.presentation,pptx," + 
+		"application/vnd.openxmlformats-officedocument.presentationml.template,potx," +
+		"application/vnd.openxmlformats-officedocument.presentationml.slideshow,ppsx," +
+		"application/x-javascript,js," +
+		"application/json,json," +
+		"audio/mpeg,mpga mpega mp2 mp3," +
+		"audio/x-wav,wav," +
+		"audio/mp4,m4a," +
+		"image/bmp,bmp," +
+		"image/gif,gif," +
+		"image/jpeg,jpeg jpg jpe," +
+		"image/photoshop,psd," +
+		"image/png,png," +
+		"image/svg+xml,svg svgz," +
+		"image/tiff,tiff tif," +
+		"text/plain,asc txt text diff log," +
+		"text/html,htm html xhtml," +
+		"text/css,css," +
+		"text/csv,csv," +
+		"text/rtf,rtf," +
+		"video/mpeg,mpeg mpg mpe m2v," +
+		"video/quicktime,qt mov," +
+		"video/mp4,mp4," +
+		"video/x-m4v,m4v," +
+		"video/x-flv,flv," +
+		"video/x-ms-wmv,wmv," +
+		"video/avi,avi," +
+		"video/webm,webm," +
+		"video/3gpp,3gp," +
+		"video/3gpp2,3g2," +
+		"video/vnd.rn-realvideo,rv," +
+		"application/vnd.oasis.opendocument.formula-template,otf," +
+		"application/octet-stream,exe";
+	
+	
+	o.extend(o, {
+		mimes: {},
+
+		extensions: {},
+
+		// Parses the default mime types string into a mimes and extensions lookup maps
+		addMimeType: function (mimeData) {
+			var items = mimeData.split(/,/), i, ii, ext;
+			
+			for (i = 0; i < items.length; i += 2) {
+				ext = items[i + 1].split(/ /);
+
+				// extension to mime lookup
+				for (ii = 0; ii < ext.length; ii++) {
+					this.mimes[ext[ii]] = items[i];
+				}
+				// mime to extension lookup
+				this.extensions[items[i]] = ext;
+			}
+		},
+
+		extList2mimes: function (filters) {
+			var self = this, ext, i, ii, y, type, mimes = [];
+			
+			// Convert extensions to mime types list
+			no_type_restriction:
+			for (i = 0; i < filters.length; i++) {
+				ext = filters[i].extensions.split(/\s*,\s*/);
+
+				for (ii = 0; ii < ext.length; ii++) {
+					
+					// If there's an asterisk in the list, then accept attribute is not required
+					if (ext[ii] === '*') {
+						mimes = [];
+						break no_type_restriction;
+					}
+					
+					type = self.mimes[ext[ii]];
+
+					if (type && !~o.inArray(type, mimes)) {
+						mimes.push(type);
+					}
+				}
+			}
+			return mimes;
+		},
+
+
+		mimes2extList: function(mimes) {
+			var self = this, exts = '', accept = [];
+			
+			mimes = o.trim(mimes);
+			
+			if (mimes !== '*') {
+				o.each(mimes.split(/\s*,\s*/), function(mime, i) {
+					if (self.extensions[mime]) {
+						exts += self.extensions[mime].join(',');
+					}
+				});
+			} else {
+				exts = mimes;	
+			}
+			
+			accept.push({
+				title: o.translate('Files'),
+				extensions: exts
+			});
+			
+			// save original mimes string
+			accept.mimes = mimes;
+							
+			return accept;
+		}
+	});
+
+	o.addMimeType(mimeData);
+}());	
+
 
 // DOM event management
 (function() {
