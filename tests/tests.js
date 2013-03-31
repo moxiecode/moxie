6c8c4dd890af18d4f99892d12b3ea5d1b3d3cc43
Davit Barbakadze <jayarjo@gmail.com>
2013-03-31 12:07:10 +0400

    Tests: Cleanup.

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,12 @@ provide([
 	[
 		{"title": "mOxie", tests: [
 			{"title": "Utils", "url": "Utils.html"},
-			{"title": "EventTarget", "url": "EventTarget.html"}
+			{"title": "EventTarget", "url": "EventTarget.html"},
+			{"title": "File", "url": "File.html"},
+			{"title": "XMLHttpRequest", "url": "XMLHttpRequest.html"},
+			{"title": "FormData", "url": "FormData.html"},
+			{"title": "Image", "url": "Image.html"},
+			{"title": "FileReader", "url": "FileReader.html"}
 		]}
 	]
 ]);
