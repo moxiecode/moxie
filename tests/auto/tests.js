972b9eec1db5c27e556b938a3621597d23c01298
Davit Barbakadze <jayarjo@gmail.com>
2013-04-04 18:36:54 +0400

    Tests: Reorganize files to adapt to coverage tests. Incorporate hacky but quite pretty and nifty test runner from TinyMCE4 (experimental?..) branch.

---
+++ tests/auto/tests.js
@@ -0,0 +1,14 @@
+{
+	"title": "mOxie", 
+	"tests": [
+		{"title": "utils.Basic", "url": "Utils/Basic.html"},
+		{"title": "utils.Dom", "url": "Utils/Dom.html"},
+		{"title": "utils.Url", "url": "Utils/Url.html"},
+		//{"title": "EventTarget", "url": "EventTarget.html"},
+		{"title": "File", "url": "File.html"},
+		{"title": "XMLHttpRequest", "url": "XMLHttpRequest.html"},
+		{"title": "FormData", "url": "FormData.html"},
+		{"title": "Image", "url": "Image.html"},
+		{"title": "FileReader", "url": "FileReader.html"}
+	]
+}
