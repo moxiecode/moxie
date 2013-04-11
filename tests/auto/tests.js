c3180854f02700acefcda76b6ec2b54ed67f1dd1
Davit Barbakadze <jayarjo@gmail.com>
2013-04-12 01:01:24 +0400

    Tests: Add Runtime test.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -5,6 +5,7 @@
 		{"title": "utils.Dom", "url": "Utils/Dom.html"},
 		{"title": "utils.Url", "url": "Utils/Url.html"},
 		//{"title": "EventTarget", "url": "EventTarget.html"},
+		{"title": "Runtime", "url": "Runtime.html"},
 		{"title": "File", "url": "File.html"},
 		{"title": "XMLHttpRequest", "url": "XMLHttpRequest.html"},
 		{"title": "FormData", "url": "FormData.html"},
