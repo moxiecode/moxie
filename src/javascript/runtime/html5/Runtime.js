0a7a9b593f4a891279da7462397478ae55451ff6
Davit Barbakadze <jayarjo@gmail.com>
2013-05-04 20:09:14 +0400

    Runtime, HTML5: Make set of capabilities extendable.

--- src/javascript/runtime/html5/Runtime.js
+++ src/javascript/runtime/html5/Runtime.js
@@ -28,55 +28,58 @@ define("moxie/runtime/html5/Runtime", [
 	function Html5Runtime(options) {
 		var I = this;
 
-		Runtime.call(this, options, (arguments[1] || type), arguments[2] || {
-			access_binary: !!(window.FileReader || window.File && window.File.getAsDataURL),
-			access_image_binary: function() {
-				return I.can('access_binary') && !!extensions.Image;
-			},
-			display_media: Env.can('create_canvas') || Env.can('use_data_uri_over32kb'),
-			do_cors: function() {
-				return !!(window.XMLHttpRequest && 'withCredentials' in new XMLHttpRequest());
-			},
-			drag_and_drop: (function() {
-				// this comes directly from Modernizr: http://www.modernizr.com/
-				var div = document.createElement('div');
-				// IE has support for drag and drop since version 5, but doesn't support dropping files from desktop
-				return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && (Env.browser !== 'IE' || Env.version > 9);
-			}()),
-			return_response_headers: true,
-			return_response_type: function(responseType) {
-				if (responseType === 'json') {
-					return true; // we can fake this one even if it's not supported
-				} else {
-					return Env.can('return_response_type', responseType);
-				}
-			},
-			report_upload_progress: function() {
-				return !!(window.XMLHttpRequest && new XMLHttpRequest().upload);
-			},
-			resize_image: function() {
-				return I.can('access_binary') && Env.can('create_canvas');
-			},
-			select_folder: Env.browser === 'Chrome' && Env.version >= 21,
-			select_multiple: !(Env.browser === 'Safari' && Env.OS === 'Windows'),
-			send_binary_string:
-				!!(window.XMLHttpRequest && (new XMLHttpRequest().sendAsBinary || (window.Uint8Array && window.ArrayBuffer))),
-			send_custom_headers: !!window.XMLHttpRequest,
-			send_multipart: function() {
-				return !!(window.XMLHttpRequest && new XMLHttpRequest().upload && window.FormData) || I.can('send_binary_string');
-			},
-			slice_blob: !!(window.File && (File.prototype.mozSlice || File.prototype.webkitSlice || File.prototype.slice)),
-			stream_upload: function() {
-				return I.can('slice_blob') && I.can('send_multipart');
-			},
-			summon_file_dialog: (function() { // yeah... some dirty sniffing here...
-				return  (Env.browser === 'Firefox' && Env.version >= 4)	||
-						(Env.browser === 'Opera' && Env.version >= 12)	||
-						!!~Basic.inArray(Env.browser, ['Chrome', 'Safari']);
-			}()),
-			upload_filesize: true
-		});
+		var caps = Basic.extend({
+				access_binary: !!(window.FileReader || window.File && window.File.getAsDataURL),
+				access_image_binary: function() {
+					return I.can('access_binary') && !!extensions.Image;
+				},
+				display_media: Env.can('create_canvas') || Env.can('use_data_uri_over32kb'),
+				do_cors: function() {
+					return !!(window.XMLHttpRequest && 'withCredentials' in new XMLHttpRequest());
+				},
+				drag_and_drop: (function() {
+					// this comes directly from Modernizr: http://www.modernizr.com/
+					var div = document.createElement('div');
+					// IE has support for drag and drop since version 5, but doesn't support dropping files from desktop
+					return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && (Env.browser !== 'IE' || Env.version > 9);
+				}()),
+				return_response_headers: true,
+				return_response_type: function(responseType) {
+					if (responseType === 'json') {
+						return true; // we can fake this one even if it's not supported
+					} else {
+						return Env.can('return_response_type', responseType);
+					}
+				},
+				report_upload_progress: function() {
+					return !!(window.XMLHttpRequest && new XMLHttpRequest().upload);
+				},
+				resize_image: function() {
+					return I.can('access_binary') && Env.can('create_canvas');
+				},
+				select_folder: Env.browser === 'Chrome' && Env.version >= 21,
+				select_multiple: !(Env.browser === 'Safari' && Env.OS === 'Windows'),
+				send_binary_string:
+					!!(window.XMLHttpRequest && (new XMLHttpRequest().sendAsBinary || (window.Uint8Array && window.ArrayBuffer))),
+				send_custom_headers: !!window.XMLHttpRequest,
+				send_multipart: function() {
+					return !!(window.XMLHttpRequest && new XMLHttpRequest().upload && window.FormData) || I.can('send_binary_string');
+				},
+				slice_blob: !!(window.File && (File.prototype.mozSlice || File.prototype.webkitSlice || File.prototype.slice)),
+				stream_upload: function() {
+					return I.can('slice_blob') && I.can('send_multipart');
+				},
+				summon_file_dialog: (function() { // yeah... some dirty sniffing here...
+					return  (Env.browser === 'Firefox' && Env.version >= 4)	||
+							(Env.browser === 'Opera' && Env.version >= 12)	||
+							!!~Basic.inArray(Env.browser, ['Chrome', 'Safari']);
+				}()),
+				upload_filesize: true
+			}, 
+			arguments[2]
+		);
 
+		Runtime.call(this, options, (arguments[1] || type), caps);
 
 		Basic.extend(this, {
 
