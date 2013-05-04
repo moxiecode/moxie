d6d8532a782e1a7e8fcbf5bcf5c2dcbe4577127a
Davit Barbakadze <jayarjo@gmail.com>
2013-05-04 20:22:03 +0400

    Tests, XHR: Fix test for browser session maintainablity by runtime.

--- tests/auto/XMLHttpRequest/session.php
+++ tests/auto/XMLHttpRequest/session.php
@@ -2,14 +2,18 @@
 
 session_start();
 
-if (!$chunking = is_numeric($_REQUEST["offset"]) && is_numeric($_REQUEST["total"])) {
-	die(json_encode(array('OK' => 0)));
+// start session
+if (isset($_REQUEST["start"]) && isset($_REQUEST["name"])) {
+	$_SESSION['fileName'] = $_REQUEST["name"];
+	die(json_encode(array('OK' => 1)));
 }
 
-if ($_REQUEST["offset"] == 0) {
-	$_SESSION['fileName'] = $_REQUEST["name"];
-} else if ($_SESSION['fileName'] != $_REQUEST["name"]) {
-	die(json_encode(array('OK' => 0)));
+// file name not saved, client doesn't send along browser cookies
+if (!isset($_SESSION['fileName']) || $_SESSION['fileName'] != $_REQUEST["name"]) {
+	die(json_encode(array(
+		'OK' => 0, 
+		'info' => empty($_SESSION) ? "Session not started" : $_REQUEST["name"] . " not registered."
+	)));
 }
 
 die(json_encode(array('OK' => 1)));
