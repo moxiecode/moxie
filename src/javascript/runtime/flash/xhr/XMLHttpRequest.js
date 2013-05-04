9931b2112c49d0509cf35748b185a3a195b51941
Davit Barbakadze <jayarjo@gmail.com>
2013-05-04 22:19:24 +0400

    Runtime, Flash/Silverlight: Request transport layer (mode) directly.

--- src/javascript/runtime/flash/xhr/XMLHttpRequest.js
+++ src/javascript/runtime/flash/xhr/XMLHttpRequest.js
@@ -29,7 +29,7 @@ define("moxie/runtime/flash/xhr/XMLHttpRequest", [
 			var target = this, self = target.getRuntime();
 
 			function send() {
-				meta.transport = self.can('send_browser_cookies') ? 'browser' : 'client';
+				meta.transport = self.getMode();
 				self.shimExec.call(target, 'XMLHttpRequest', 'send', meta, data);
 			}
 
