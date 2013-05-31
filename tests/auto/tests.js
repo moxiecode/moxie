6afd2c3d06ede8c29ffcb0ece5d9dfceb0169ece
Davit Barbakadze <jayarjo@gmail.com>
2013-05-31 20:14:43 +0400

    Tests, EventTarget: Test sync/async events.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -6,7 +6,7 @@
 		{"title": "utils.Url", "url": "Utils/Url.html"},
 		{"title": "utils.Mime", "url": "Utils/Mime.html"},
 		{"title": "utils.I18n", "url": "Utils/I18n.html"},
-		//{"title": "EventTarget", "url": "EventTarget.html"},
+		{"title": "EventTarget", "url": "EventTarget.html"},
 		{"title": "Runtime", "url": "Runtime.html"},
 		{"title": "Blob", "url": "Blob.html"},
 		{"title": "File", "url": "File.html"},
