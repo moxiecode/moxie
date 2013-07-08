86ce1f949e431c922f7d541531498ba03f4cac04
Davit Barbakadze <jayarjo@gmail.com>
2013-07-08 22:29:26 +0400

    FileInput, HTML5: For IE only, reset file input's value by reinserting input element into the DOM. Related #811.
    In IE input[type="file"] is read-only so the only way to reset it is to
    reinsert it.

--- src/javascript/runtime/html5/file/FileInput.js
+++ src/javascript/runtime/html5/file/FileInput.js
@@ -17,15 +17,16 @@ define("moxie/runtime/html5/file/FileInput", [
 	"moxie/core/utils/Basic",
 	"moxie/core/utils/Dom",
 	"moxie/core/utils/Events",
-	"moxie/core/utils/Mime"
-], function(extensions, Basic, Dom, Events, Mime) {
+	"moxie/core/utils/Mime",
+	"moxie/core/utils/Env"
+], function(extensions, Basic, Dom, Events, Mime, Env) {
 	
 	function FileInput() {
 		var _files = [], _options;
 
 		Basic.extend(this, {
 			init: function(options) {
-				var comp = this, I = comp.getRuntime(), input, shimContainer, mimes;
+				var comp = this, I = comp.getRuntime(), input, shimContainer, mimes, browseButton, zIndex, top;
 
 				_options = options;
 				_files = [];
@@ -51,53 +52,51 @@ define("moxie/runtime/html5/file/FileInput", [
 					height: '100%'
 				});
 
-				(function() {
-					var browseButton, zIndex, top;
 
-					browseButton = Dom.get(_options.browse_button);
+				browseButton = Dom.get(_options.browse_button);
 
-					// Route click event to the input[type=file] element for browsers that support such behavior
-					if (I.can('summon_file_dialog')) {
-						if (Dom.getStyle(browseButton, 'position') === 'static') {
-							browseButton.style.position = 'relative';
-						}
-
-						zIndex = parseInt(Dom.getStyle(browseButton, 'z-index'), 10) || 1;
-
-						browseButton.style.zIndex = zIndex;
-						shimContainer.style.zIndex = zIndex - 1;
-
-						Events.addEvent(browseButton, 'click', function(e) {
-							if (input && !input.disabled) { // for some reason FF (up to 8.0.1 so far) lets to click disabled input[type=file]
-								input.click();
-							}
-							e.preventDefault();
-						}, comp.uid);
+				// Route click event to the input[type=file] element for browsers that support such behavior
+				if (I.can('summon_file_dialog')) {
+					if (Dom.getStyle(browseButton, 'position') === 'static') {
+						browseButton.style.position = 'relative';
 					}
 
-					/* Since we have to place input[type=file] on top of the browse_button for some browsers,
-					browse_button loses interactivity, so we restore it here */
-					top = I.can('summon_file_dialog') ? browseButton : shimContainer;
+					zIndex = parseInt(Dom.getStyle(browseButton, 'z-index'), 10) || 1;
 
-					Events.addEvent(top, 'mouseover', function() {
-						comp.trigger('mouseenter');
+					browseButton.style.zIndex = zIndex;
+					shimContainer.style.zIndex = zIndex - 1;
+
+					Events.addEvent(browseButton, 'click', function(e) {
+						var input = Dom.get(I.uid);
+						if (input && !input.disabled) { // for some reason FF (up to 8.0.1 so far) lets to click disabled input[type=file]
+							input.click();
+						}
+						e.preventDefault();
 					}, comp.uid);
+				}
 
-					Events.addEvent(top, 'mouseout', function() {
-						comp.trigger('mouseleave');
-					}, comp.uid);
+				/* Since we have to place input[type=file] on top of the browse_button for some browsers,
+				browse_button loses interactivity, so we restore it here */
+				top = I.can('summon_file_dialog') ? browseButton : shimContainer;
 
-					Events.addEvent(top, 'mousedown', function() {
-						comp.trigger('mousedown');
-					}, comp.uid);
+				Events.addEvent(top, 'mouseover', function() {
+					comp.trigger('mouseenter');
+				}, comp.uid);
 
-					Events.addEvent(Dom.get(_options.container), 'mouseup', function() {
-						comp.trigger('mouseup');
-					}, comp.uid);
+				Events.addEvent(top, 'mouseout', function() {
+					comp.trigger('mouseleave');
+				}, comp.uid);
 
-				}());
+				Events.addEvent(top, 'mousedown', function() {
+					comp.trigger('mousedown');
+				}, comp.uid);
 
-				input.onchange = function() { // there should be only one handler for this
+				Events.addEvent(Dom.get(_options.container), 'mouseup', function() {
+					comp.trigger('mouseup');
+				}, comp.uid);
+
+
+				input.onchange = function onChange() { // there should be only one handler for this
 					_files = [];
 
 					if (_options.directory) {
@@ -112,7 +111,14 @@ define("moxie/runtime/html5/file/FileInput", [
 					}
 
 					// clearing the value enables the user to select the same file again if they want to
-					this.value = '';
+					if (Env.browser !== 'IE') {
+						this.value = '';
+					} else {
+						// in IE input[type="file"] is read-only so the only way to reset it is to re-insert it
+						var clone = this.cloneNode(true);
+						this.parentNode.replaceChild(clone, this);
+						clone.onchange = onChange;
+					}
 					comp.trigger('change');
 				};
 
@@ -121,6 +127,8 @@ define("moxie/runtime/html5/file/FileInput", [
 					type: 'ready',
 					async: true
 				});
+
+				shimContainer = null;
 			},
 
 			getFiles: function() {
