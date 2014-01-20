64fc1a82458e9faa1772dfd98328f89ff962d08c
Jeroen van den Heuvel <jeroen.van.den.heuvel@werkspot.nl>
2014-01-20 18:28:59 +0100

    Added missing document in front of createElement

--- src/javascript/runtime/html5/Runtime.js
+++ src/javascript/runtime/html5/Runtime.js
@@ -84,7 +84,7 @@ define("moxie/runtime/html5/Runtime", [
 					if(navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
 						return false;
 					}
-					var elem = createElement('input');
+					var elem = document.createElement('input');
 					elem.type = 'file';
 					return !elem.disabled;
 				}()),
