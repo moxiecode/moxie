f657a81a8b245fd7ca86833bc77518c39d39bed1
Davit Barbakadze <jayarjo@gmail.com>
2014-02-05 13:52:47 +0400

    Flash, Runtime: Fix slice_blob is only possible in browser mode.

--- src/javascript/runtime/flash/Runtime.js
+++ src/javascript/runtime/flash/Runtime.js
@@ -97,7 +97,9 @@ define("moxie/runtime/flash/Runtime", [
 				return value && I.mode === 'browser';
 			},
 			send_multipart: Runtime.capTrue,
-			slice_blob: Runtime.capTrue,
+			slice_blob: function(value) {
+				return value && I.mode === 'browser';
+			},
 			stream_upload: function(value) {
 				return value && I.mode === 'browser';
 			},
