11c7cb04e241f16aa866f3ae3883483562a0eec2
Davit Barbakadze <jayarjo@gmail.com>
2014-05-16 18:09:44 +0400

    FileInput, Flash/Silverlight: Port to new logic.

--- src/javascript/runtime/flash/file/FileInput.js
+++ src/javascript/runtime/flash/file/FileInput.js
@@ -13,16 +13,29 @@
 @private
 */
 define("moxie/runtime/flash/file/FileInput", [
-	"moxie/runtime/flash/Runtime"
-], function(extensions) {
+	"moxie/runtime/flash/Runtime",
+	"moxie/file/File",
+	"moxie/core/utils/Basic"
+], function(extensions, File, Basic) {
 	
 	var FileInput = {		
 		init: function(options) {
+			var comp = this, I = this.getRuntime();
+
+			this.bind("Change", function() {
+				var files = I.shimExec.call(comp, 'FileInput', 'getFiles');
+				comp.files = [];
+				Basic.each(files, function(file) {
+					comp.files.push(new File(I.ruid, file));
+				});
+			}, 999);
+
 			this.getRuntime().shimExec.call(this, 'FileInput', 'init', {
 				name: options.name,
 				accept: options.accept,
 				multiple: options.multiple
 			});
+
 			this.trigger('ready');
 		}
 	};
