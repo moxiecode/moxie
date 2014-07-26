20cc512c27184d29cb9a7367cfa9cbdb7b38024e
Davit Barbakadze <jayarjo@gmail.com>
2014-07-26 19:35:15 +0400

    Tests, GoogleDrive/Dropbox: Add some.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -3,5 +3,7 @@
 	"tests": [
 		{"title": "Runtime", "url": "Runtime.html"},
 		{"title": "FileInput", "url": "FileInput.html"},
+		{"title": "GoogleDrive: FileInput", "url": "runtimes/googledrive/FileInput.html"},
+		{"title": "Dropbox: FileInput", "url": "runtimes/dropbox/FileInput.html"}
 	]
 }
