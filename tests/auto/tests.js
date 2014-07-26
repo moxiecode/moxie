db33acde562dc361ca57b3fff9f5fea395a543f4
Davit Barbakadze <jayarjo@gmail.com>
2014-07-26 19:35:08 +0400

    Tests: Remove the tests that require a fix.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -1,21 +1,7 @@
 {
-	"title": "mOxie", 
+	"title": "Automated Tests", 
 	"tests": [
-		{"title": "utils.Basic", "url": "Utils/Basic.html"},
-		{"title": "utils.Dom", "url": "Utils/Dom.html"},
-		{"title": "utils.Url", "url": "Utils/Url.html"},
-		{"title": "utils.Mime", "url": "Utils/Mime.html"},
-		{"title": "utils.I18n", "url": "Utils/I18n.html"},
-		{"title": "EventTarget", "url": "EventTarget.html"},
 		{"title": "Runtime", "url": "Runtime.html"},
-		{"title": "Blob", "url": "Blob.html"},
-		{"title": "File", "url": "File.html"},
-		{"title": "FormData", "url": "FormData.html"},
-		{"title": "Image", "url": "Image.html"},
 		{"title": "FileInput", "url": "FileInput.html"},
-		{"title": "FileReader", "url": "FileReader.html"},
-		{"title": "XHR", "url": "XHR.html"},
-		{"title": "XHR CORS", "url": "XHR-CORS.html"}
-	
 	]
 }
