d08e9fce41d40942af7c6658a47d21a841d90156
Davit Barbakadze <jayarjo@gmail.com>
2014-08-17 17:59:16 +0400

    Tests, FileReader: Fix. Add readAsText() test.
    Related to #97.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -3,6 +3,7 @@
 	"tests": [
 		{"title": "Runtime", "url": "Runtime.html"},
 		{"title": "FileInput", "url": "FileInput.html"},
+		{"title": "FileReader", "url": "FileReader.html"},
 		{"title": "HTML4: FileInput", "url": "runtimes/html4/FileInput.html"},
 		{"title": "GoogleDrive: FileInput", "url": "runtimes/googledrive/FileInput.html"},
 		{"title": "Dropbox: FileInput", "url": "runtimes/dropbox/FileInput.html"}
