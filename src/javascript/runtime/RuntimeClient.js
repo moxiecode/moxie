3fccce07a05dc5c504b9369611da939cd2468b5c
Tarjei Huse <tarjei.huse@gmail.com>
2014-10-02 11:58:46 +0200

    Add guard to setTimeout
    As the runtime might have been destroyed before the function executes, causing errors.

--- src/javascript/runtime/RuntimeClient.js
+++ src/javascript/runtime/RuntimeClient.js
@@ -59,9 +59,12 @@ define('moxie/runtime/RuntimeClient', [
 
 						// jailbreak ...
 						setTimeout(function() {
-							runtime.clients++;
-							// this will be triggered on component
-							comp.trigger('RuntimeInit', runtime);
+							// runtime might have been destryed before init.
+							if (runtime) {
+								runtime.clients++;
+								// this will be triggered on component
+								comp.trigger('RuntimeInit', runtime);
+							}
 						}, 1);
 					});
 
