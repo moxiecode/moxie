e323440a23b50f56aef593da0173a7b522af7430
Davit Barbakadze <jayarjo@gmail.com>
2014-12-29 12:12:44 +0400

    Tests: Stick to specific QUnit version (1.14.0)... and stop upgrade madness.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -2,10 +2,17 @@
 	"title": "Automated Tests", 
 	"tests": [
 		{"title": "Utils/Url", "url": "Utils/Url.html"},
+		{"title": "Utils/Basic", "url": "Utils/Basic.html"},
+		{"title": "Utils/I18n", "url": "Utils/I18n.html"},
+		{"title": "Utils/Mime", "url": "Utils/Mime.html"},
 		{"title": "Runtime", "url": "Runtime.html"},
+		{"title": "FormData", "url": "FormData.html"},
+		{"title": "Image", "url": "Image.html"},
 		{"title": "File", "url": "File.html"},
 		{"title": "FileInput", "url": "FileInput.html"},
 		{"title": "FileReader", "url": "FileReader.html"},
+		{"title": "XHR", "url": "XHR.html"},
+		{"title": "XHR-CORS", "url": "XHR-CORS.html"},
 		{"title": "HTML4: FileInput", "url": "runtimes/html4/FileInput.html"},
 		{"title": "GoogleDrive: FileInput", "url": "runtimes/googledrive/FileInput.html"},
 		{"title": "Dropbox: FileInput", "url": "runtimes/dropbox/FileInput.html"}
