7beb33192511a497f7ffdc8a69490dc07170244a
jayarjo <jayarjo@gmail.com>
2015-05-10 23:03:17 +0400

    Tests, Flash: swf shouldn't accept any parameters via query string when loaded directly.

--- tests/auto/tests.js
+++ tests/auto/tests.js
@@ -15,7 +15,8 @@
 		{"title": "FileInput", "url": "FileInput.html"},
 		{"title": "FileReader", "url": "FileReader.html"},
 		{"title": "XHR", "url": "XHR.html"},
-		{"title": "XHR CORS", "url": "XHR-CORS.html"}
+		{"title": "XHR CORS", "url": "XHR-CORS.html"},
+		{"title": "Flash: Runtime", "url": "runtimes/flash/Runtime.html"}
 	
 	]
 }
