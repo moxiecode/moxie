ff47fbd41d4d6e19b34a65a79329c7abf938281e
Davit Barbakadze <jayarjo@gmail.com>
2015-05-24 00:02:08 +0400

    Fix indenting.

--- src/javascript/core/utils/Env.js
+++ src/javascript/core/utils/Env.js
@@ -597,8 +597,8 @@ define("moxie/core/utils/Env", [
 
 				use_fileinput: function() {
 					if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
-				        return false;
-				    }
+						return false;
+					}
 
 					var el = document.createElement('input');
 					el.setAttribute('type', 'file');
