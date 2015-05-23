5d8750a3fe8024c6a103d34b15a28a24379e2e4e
Davit Barbakadze <jayarjo@gmail.com>
2015-05-24 00:00:04 +0400

    Runtime, HTML4/HTML5: summon_file_dialog should depend on select_file. Related #67.

--- src/javascript/runtime/html5/Runtime.js
+++ src/javascript/runtime/html5/Runtime.js
@@ -81,10 +81,12 @@ define("moxie/runtime/html5/Runtime", [
 					return I.can('slice_blob') && I.can('send_multipart');
 				},
 				summon_file_dialog: Test(function() { // yeah... some dirty sniffing here...
-					return (Env.browser === 'Firefox' && Env.version >= 4) ||
+					return I.can('select_file') && (
+						(Env.browser === 'Firefox' && Env.version >= 4) ||
 						(Env.browser === 'Opera' && Env.version >= 12) ||
 						(Env.browser === 'IE' && Env.version >= 10) ||
-						!!~Basic.inArray(Env.browser, ['Chrome', 'Chromium', 'Safari']);
+						!!~Basic.inArray(Env.browser, ['Chrome', 'Safari'])
+					);
 				}()),
 				upload_filesize: True
 			}, 
