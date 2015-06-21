dcf588c9d2295b413a8d3e11b002f2947b0c2dbb
Davit Barbakadze <jayarjo@gmail.com>
2015-06-21 16:42:58 +0400

    Tests: Move dev only tests into a separate section.

---
+++ tests/auto/dev-tests.js
@@ -0,0 +1,8 @@
+{
+	"title": "Dev Tests", 
+	"tests": [
+		{"title": "HTML5: BinaryReader", "url": "runtimes/html5/BinaryReader.html"},
+		{"title": "HTML5: ExifParser", "url": "runtimes/html5/ExifParser.html"},
+		{"title": "HTML5: ExifParser, Problematic JPEGs", "url": "runtimes/html5/ProblematicJPEGs.html"}
+	]
+}
