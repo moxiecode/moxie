a4dadb0a2a070fa2ed65f9da06071d1fe2d4e500
Davit Barbakadze <jayarjo@gmail.com>
2016-08-16 10:01:24 -0700

    Silverlight: Add company name to assembly.

--- src/silverlight/Properties/AssemblyInfo.cs
+++ src/silverlight/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Moxie")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ephox")]
 [assembly: AssemblyProduct("Moxie")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
